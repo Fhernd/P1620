aa6c61710dd977e459bcd328e916aa4d61e155eb
John Ortiz Ordoñez <johnortizo@outlook.com>
2016-03-26 21:11:13 -0500

    Pregunta P1620

---
+++ P1620/P1620.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1620
+{
+    public partial class P1620 : Form
+    {
+        public P1620()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumerico_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar);
+        }
+    }
+}
